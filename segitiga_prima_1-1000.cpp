56cb86098d3c464238e8f6c7895d57c24eedecbd
eviepk12 <62878539+eviepk12@users.noreply.github.com>
2021-10-04 23:02:42 +0700

    Add files via upload

---
+++ segitiga_prima_1-1000.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+
+    int i,j,count=0;
+
+    for (i = 1; i<=1000; i++)
+    {
+        for (j = 2; j<i; j++)
+        {
+            if (i % j==0)
+            {
+                count++;
+                break;
+                cout << count++ << "\n ";
+            }
+        }
+        if (count==0 && i !=1)
+         cout << i << "\t ";
+         count=0;
+    }
+
+    cin.get();
+    return 0;
+}
